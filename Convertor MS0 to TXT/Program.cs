87638fc9da810129b1fe8be464fcfd98749c0d30
Yuriy Z. <LodestaRgr@yandex.ru>
2016-05-02 18:39:42 +0300

    Добавьте файлы проекта.

---
+++ Convertor MS0 to TXT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convertor_MS0_to_TXT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
